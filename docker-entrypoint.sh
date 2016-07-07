8cc7b563af92b8637a26bbc750ed758d1635a810
Mickaël Canévet <mickael.canevet@camptocamp.com>
2016-07-07 09:43:31 +0200

    Get OpenVAS SCAP database at runtime

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,5 +6,8 @@ openvasmd --rebuild
 echo "Creating an administrator user for OpenVAS..."
 openvasmd --create-user=admin
 
+echo "Getting OpenVAS SCAP database..."
+openvas-scapdata-sync
+
 echo "Launching openvas-manager..."
 exec /usr/local/sbin/openvasmd $*
