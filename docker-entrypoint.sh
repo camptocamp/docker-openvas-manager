ee2e9e931bc61db14b33e42504dbf26b920308b8
Mickaël Canévet <mickael.canevet@camptocamp.com>
2016-07-07 09:19:16 +0200

    Add logging messages

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+echo "Initializing the Database..."
 openvasmd --rebuild
+
+echo "Creating an administrator user for OpenVAS..."
 openvasmd --create-user=admin
+
+echo "Launching openvas-manager..."
 exec /usr/local/sbin/openvasmd $*
