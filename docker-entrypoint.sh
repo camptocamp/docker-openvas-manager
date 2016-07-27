4229d94a3e3091b2b4b5bea3397e13b73c65d92d
Mickaël Canévet <mickael.canevet@camptocamp.com>
2016-07-27 11:37:04 +0200

    Allow setting OPENVAS_ADMIN_USER and OPENVAS_ADMIN_PASSWORD

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,11 @@ echo "Initializing the Database..."
 openvasmd --rebuild
 
 echo "Creating an administrator user for OpenVAS..."
-openvasmd --create-user=admin
+USER=${OPENVAS_ADMIN_USER:-admin}
+openvasmd --create-user=${USER}
+if [ -n "${OPENVAS_ADMIN_PASSWORD}" ] ; then
+  openvasmd --new-password="${OPENVAS_ADMIN_PASSWORD}" --user=${USER}
+fi
 
 echo "Getting OpenVAS SCAP database..."
 openvas-scapdata-sync
